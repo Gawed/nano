67ff0e0a32e9136a1b520dbba6429cf19725d5e0
Gawed <sdzy696@gmail.com>
2025-03-04 10:18:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,18 @@
+import smbus2
 import paho.mqtt.client as mqtt
-import serial
-import time
 
-# MQTT 配置
-MQTT_BROKER = "192.168.1.205"  # 如果你的 MQTT Broker 在树莓派上，使用 "localhost"
-MQTT_TOPIC = "buzzer"
-MQTT_PORT = 1883
+I2C_ADDR = 0x08  # Arduino 的 I2C 地址
+bus = smbus2.SMBus(1)  # Raspberry Pi 默认使用 I2C-1
 
-# 串口配置
-SERIAL_PORT = '/dev/ttyUSB0'  # 根据树莓派与 Arduino 连接的串口端口修改
-BAUD_RATE = 9600
+def on_message(client, userdata, msg):
+    if msg.topic == "buzzer/ON" and msg.payload.decode() == "O":
+        bus.write_byte(I2C_ADDR, ord('O'))  # 发送 ASCII 'O' 给 Arduino
+        print("Sent 'O' to Arduino via I2C")
 
-# 初始化串口
-ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
-
-# MQTT 回调函数
-def on_message(client, userdata, message):
-    # 处理接收到的消息
-    msg = message.payload.decode()
-    if msg == "ON":
-        # 向 Arduino 发送命令
-        ser.write(b"ON")  # 向 Arduino 发送 'ON' 命令
-
-# 配置 MQTT 客户端
 client = mqtt.Client()
 client.on_message = on_message
 
-# 连接到 MQTT Broker
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.connect("mqtt_broker_address", 1883, 60)  # 修改 MQTT 服务器地址
+client.subscribe("buzzer/ON")
 
-# 订阅主题
-client.subscribe(MQTT_TOPIC)
-
-# 开始 MQTT 客户端循环
-client.loop_start()
-
-# 保持程序运行
-while True:
-    time.sleep(1)
+client.loop_forever()
