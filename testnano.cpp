7b9266d0f0963267d4888b03f24638a1fe727a6d
Gawed <sdzy696@gmail.com>
2025-03-12 13:59:27 +0100

    Update testnano.cpp

--- testnano.cpp
+++ testnano.cpp
@@ -1,26 +1,37 @@
 #include <Wire.h>
 
-#define I2C_ADDRESS 0x08  // Arduino 的 I2C 地址
+#define I2C_ADDRESS 0x08
 #define BUZZER_PIN 10
 
+volatile bool buzzerOn = false;  //  volatile pour I²C  signal
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
-  Wire.begin(I2C_ADDRESS);  // 设定 Arduino 作为 I²C 从设备
+  Serial.begin(9600); 
+  Wire.begin(I2C_ADDRESS);
+  Serial.print("I2C address est: ");
+  Serial.println(I2C_ADDRESS, HEX);
   Wire.onReceive(receiveEvent);
+  Serial.println("Arduino I2C Initialized");
 }
 
 void loop() {
-  
+  if (buzzerOn) {
+    Serial.println("Buzzer Activated!");  
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_PIN, LOW);
+    Serial.println("Buzzer Deactivated!"); 
+    buzzerOn = false;
+  }
 }
 
-
 void receiveEvent(int numBytes) {
+  Serial.print("Received I2C data: ");
   while (Wire.available()) {
     char c = Wire.read();
+    Serial.println(c);  
     if (c == 'O') {
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(1000);
-      digitalWrite(BUZZER_PIN, LOW);
+      buzzerOn = true;
     }
   }
 }
