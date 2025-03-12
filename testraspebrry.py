4b8ed745364a4a8eaea46cbc6d638d265c28d5b6
Gawed <sdzy696@gmail.com>
2025-03-12 14:23:39 +0100

    Update testraspebrry.py

--- testraspebrry.py
+++ testraspebrry.py
@@ -1,42 +1,40 @@
-import smbus2
-import paho.mqtt.client as mqtt
+#include <Wire.h>
 
-I2C_ADDR = 0x08  # Arduino Nano  I2C 
-bus = smbus2.SMBus(1)  # Raspberry Pi I2C-1 
+#define I2C_ADDRESS 0x08
+#define BUZZER_PIN 10
 
-# MQTT 
-MQTT_BROKER = "192.168.1.205"  
-MQTT_PORT = 1883 
-MQTT_TOPIC = "buzzer/ON"
+volatile bool buzzerOn = false;  //  volatile pour I²C  signal
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+  Serial.begin(9600); 
+  Wire.begin(I2C_ADDRESS);
+  Serial.print("I2C address est: ");
+  Serial.println(I2C_ADDRESS, HEX);
+  Wire.onReceive(receiveEvent);
+  Serial.println("Arduino I2C Initialized");
+}
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT broker")
-        client.subscribe(MQTT_TOPIC)
-    else:
-        print(f"Failed to connect, return code {rc}")
+void loop() {
+  if (buzzerOn) {
+    Serial.println("Buzzer Activated!");  
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+}
 
-def on_message(client, userdata, msg):
-    payload = msg.payload.decode().strip()
-    print(f"Received message: {payload}")
-    if msg.topic == MQTT_TOPIC and payload == "ON":
-        try:
-            bus.write_byte(I2C_ADDR, ord('ON'))  #  ASCII 'O'  Arduino
-            print("Sent 'ON' to Arduino via I2C")
-        except Exception as e:
-            print(f"Failed to send I2C message: {e}")
-    if msg.topic == MQTT_TOPIC and payload == "OFF":
-        try:
-            bus.write_byte(I2C_ADDR, ord('OFF'))  #  ASCII 'OFF'  Arduino
-            print("Sent 'OFF' to Arduino via I2C")
-        except Exception as e:
-            print(f"Failed to send I2C message: {e}")
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+void receiveEvent(int numBytes) {
+  Serial.print("Received I2C data: ");
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.println(c);  
+    if (c == 'ON') {
+      buzzerOn = true;
+    }
+    else if (c == 'OFF'){
+      buzzerOn = false;
+      Serial.println("Buzzer Deactivated!"); 
+    }
 
-try:
-    client.connect(MQTT_BROKER, MQTT_PORT, 60)  
-    client.loop_forever()  
-except Exception as e:
-    print(f"MQTT connection error: {e}")
+  }
+}
